06c8af7cc2a0e5f57befba87978ff30228445eec
jeugeniojunior <jeugenio.junior@gmail.com>
2022-04-05 06:15:25 -0300

    alteração na forma de resolver exercico 2 - classe

--- Exercicios/Com Classes/Exercicio 2.py
+++ Exercicios/Com Classes/Exercicio 2.py
@@ -1,18 +1,18 @@
 class Quadrado:
-    def __init__(self):
-        self.tamanho = 10
+    def __init__(self, tamanho = 4):
+        self.tamanho = tamanho
 
     def muda_tamanho(self, tamanho):
         self.tamanho = tamanho
 
     def area(self):
-        return (self.tamanho) *2
+        return (self.tamanho) ** 2
 
 
 quadrado1 = Quadrado()
 quadrado2 = Quadrado()
 
-quadrado2.muda_tamanho(15)
+quadrado2.muda_tamanho(5)
 
 print(quadrado1.area())
 print(quadrado2.area())
