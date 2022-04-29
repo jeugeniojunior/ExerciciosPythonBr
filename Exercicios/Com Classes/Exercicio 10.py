a6bd159176e798362329d4b432a08cf04567b60d
jeugeniojunior <jeugenio.junior@gmail.com>
2022-04-29 06:30:53 -0300

    Implementado repetiação no Exercicio 10 em classes

--- Exercicios/Com Classes/Exercicio 10.py
+++ Exercicios/Com Classes/Exercicio 10.py
@@ -28,37 +28,41 @@ class BombaCombustivel:
         else:
             print('Sai pra lá ladrão')
 
+while True:
 
-operacao = input('Qual operação você deseja fazer (A) - Abartecer (R) - Reabastecer bomba: ').upper()
+    operacao = input('Qual operação você deseja fazer (A)-Abartecer   (R)-Reabastecer bomba  (S)-Sair: ').upper()
 
-if operacao == 'R':
-    tipo = input('Tipo de combustivel ( G - gasolina, A - alcool): ').upper()
-    if tipo == 'G':
-        bomba = BombaCombustivel('gasolina', 4.59, 100.0)
-        litros_adicionados = float(input('Quantidade a ser colocada na bomba: '))
-        bomba.adicionar_combustivel(litros_adicionados)
+    if operacao == 'R':
+        tipo = input('Tipo de combustivel ( G - gasolina, A - alcool): ').upper()
+        if tipo == 'G':
+            bomba = BombaCombustivel('gasolina', 4.59, 100.0)
+            litros_adicionados = float(input('Quantidade a ser colocada na bomba: '))
+            bomba.adicionar_combustivel(litros_adicionados)
 
-    elif tipo == 'A':
-        bomba = BombaCombustivel('alcool', 2.00, 50.0)
-        litros_adicionados = float(input('Quantidade a ser colocada na bomba: '))
-        bomba.adicionar_combustivel(litros_adicionados)
+        elif tipo == 'A':
+            bomba = BombaCombustivel('alcool', 2.00, 50.0)
+            litros_adicionados = float(input('Quantidade a ser colocada na bomba: '))
+            bomba.adicionar_combustivel(litros_adicionados)
 
 
-else:
-    tipo = input('Tipo de combustivel ( G - gasolina, A - alcool: ').upper()
-    if tipo == 'G':
-       bomba = BombaCombustivel('gasolina', 4.59, 100.0)
-    elif tipo == 'A':
-        bomba = BombaCombustivel('alcool', 2.00, 50.0)
+    else:
+        tipo = input('Tipo de combustivel ( G - gasolina, A - alcool: ').upper()
+        if tipo == 'G':
+           bomba = BombaCombustivel('gasolina', 4.59, 100.0)
+        elif tipo == 'A':
+            bomba = BombaCombustivel('alcool', 2.00, 50.0)
 
-    condicao_abastecimento = input('Condição de abastecimento (D - dinheiro ou L - litros: ').upper()
+        condicao_abastecimento = input('Condição de abastecimento (D - dinheiro ou L - litros: ').upper()
 
-    if condicao_abastecimento == 'D':
-        valor_gasto = float(input('Digite o valor: '))
-        bomba.abastecer_por_valor (valor_gasto)
-    elif condicao_abastecimento == 'L':
-        qtd_desejada = float(input('Digite a quantidade: '))
-        bomba.abastecer_por_litros (qtd_desejada)
+        if condicao_abastecimento == 'D':
+            valor_gasto = float(input('Digite o valor: '))
+            bomba.abastecer_por_valor (valor_gasto)
+        elif condicao_abastecimento == 'L':
+            qtd_desejada = float(input('Digite a quantidade: '))
+            bomba.abastecer_por_litros (qtd_desejada)
+
+    if operacao == 'S':
+        break
 
 
 
